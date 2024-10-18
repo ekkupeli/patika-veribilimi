12aa10d8393f8a97de417cd6a0521b01085bf7bd
Emre Kaan Küpeli <92442286+ekkupeli@users.noreply.github.com>
2024-10-18 16:01:51 +0300

    Update finalproject.py for solving isinstance() must type arg
    The error that the second argument of the isinstance() function must be in type format has been resolved

--- beginner/pythontemel/finalproject.py
+++ beginner/pythontemel/finalproject.py
@@ -1,16 +1,16 @@
 #Python Temel Eğitim Final Projesi
-def flatten(list):
+def flatten(n_list):
     flat_list = []
-    for item in list:
+    for item in n_list:
         if isinstance(item, list):
             flat_list.extend(flatten(item))  # Eğer item bir listeyse, rekürsif olarak düzleştir
         else:
             flat_list.append(item)  # Aksi halde elemanı doğrudan ekle
     return flat_list
 
-def reverse_nested(list):
+def reverse_nested(n_list):
     reversed_list = []
-    for item in reversed(list):  # Listenin elemanlarını ters sırada al
+    for item in reversed(n_list):  # Listenin elemanlarını ters sırada al
         if isinstance(item, list):
             reversed_list.append(reverse_nested(item))  # Eğer item bir listeyse, rekürsif olarak tersine çevir
         else:
